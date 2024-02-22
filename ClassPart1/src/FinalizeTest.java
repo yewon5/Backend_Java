9007a331b39e5fdf1f538cdf3297b2f3983e2c9a
yewon5 <ik_324@naver.com>
2024-02-22 18:17:14 +0900

    ClassPart1

--- ClassPart1/src/FinalizeTest.java
+++ ClassPart1/src/FinalizeTest.java
@@ -1,4 +1,4 @@
-class FDemo {
+class FDemo { //자바는 기본적으로 상속을 받는 클래스가 존재한다.
 	int count;
 	
 	FDemo(int i) { //객체를 생성할 때 마다 count 변수에 들어간다.
@@ -7,7 +7,7 @@ class FDemo {
 	
 	protected void finalize() {
 		System.out.println("Garbage Collection 가동 : " + count);
-	}
+	} //오버라이딩
 }
 
 public class FinalizeTest {
